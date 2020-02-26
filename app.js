fb30f2510ef0313b5e77af350ad248517d7f1a10
Cbustamante777 <cra.bustamante@alumnos.duoc.cl>
2020-02-25 23:09:09 -0300

    solicitud url funcionado desde la terminal uso de yargs y axios

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const axios = require('axios')
+
+
 const argv = require('yargs').options({
     direccion:{
         alias:'d',
@@ -9,3 +12,22 @@ const argv = require('yargs').options({
 console.log(argv.direccion);
 
 //para probar (node app -d "santiago de chile");
+
+const encodeURL = encodeURI(argv.direccion);
+console.log(encodeURL);
+
+const instance = axios.create({
+    //probada desde postman
+    
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeURL}`,
+    headers: {'X-RapidAPI-Key': '0bae536c00msha9339428abc9d2dp1fd0fejsn27761ee01bd4'}
+  });
+
+  instance.get()
+  .then(resp => {
+      //solo va imprimir el primero en postman sale como se llaman los paramentros
+      console.log(resp.data.Results[0]);
+  })
+  .catch(err =>{
+      console.log('error',err);
+  })
